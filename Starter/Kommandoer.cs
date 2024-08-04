55094593e18344eaed1c6747770ab57802e93853
Tobias Lemming <toblem96@gmail.com>
2024-08-05 00:08:40 +0200

    Oppstart av View

---
+++ Starter/Kommandoer.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.GraphicsSystem;
+using System.Windows.Interop;
+
+
+
+[assembly: CommandClass(typeof(Fargemannen_GR.MyCommands))]
+
+namespace Fargemannen_GR
+{
+    public class MyCommands
+    {
+
+        [CommandMethod("Fargemannen_GR")]
+        public void KjørFargemannen()
+        {
+
+            View.MainWindow MainWindow = new View.MainWindow();
+
+            WindowInteropHelper helper = new WindowInteropHelper(MainWindow)
+            {
+                Owner = Application.MainWindow.Handle
+            };
+            MainWindow.Show();
+        }
+    }
+}
---
+++ View/Filopplasting/FilopplastingMain.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fargemannen_GR.View.Filopplasting
+{
+    /// <summary>
+    /// Interaction logic for FilopplastingMain.xaml
+    /// </summary>
+    public partial class FilopplastingMain : Window
+    {
+        public FilopplastingMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
